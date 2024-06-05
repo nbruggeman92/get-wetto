25936bc48d9b10442d33547378f6d0cb7e439e4f
Clifton <cliftonreed1986@gmail.com>
2024-06-05 13:23:32 -0700

    added range to modle and animal seeds

--- server/seeders/animalSeeds.js
+++ server/seeders/animalSeeds.js
@@ -3,31 +3,37 @@ const animalSeedData = [
       name: "Bear",
       description:
         "You are a Bear! Your strength and determination are unmatched. You tackle challenges head-on with a fearless attitude. While some may see you as aggressive, you simply have a no-nonsense approach to life. Your determination and power make you a force to be reckoned with.",
+      range: [12, 21],
     },
     {
       name: "Jaguar",
       description:
         "Congratulations! You're a Jaguar! Confident and graceful, you move through life with purpose and style. You exude self-assurance in everything you do, inspiring those around you to strive for greatness. Your confidence is your superpower, propelling you towards success in all aspects of life.",
+      range: [22, 31],
     },
     {
       name: "Eagle",
       description:
         "You're an Eagle! With a keen eye for detail and a love for the unconventional, you soar above the rest with your unique perspective. You're not afraid to stand out from the crowd and march to the beat of your own drum. Your independent spirit and creative flair make you a true trendsetter.",
+      range: [32, 41],
     },
     {
       name: "Dolphin",
       description:
         "You are a Dolphin! Intelligent and sociable, you thrive in dynamic environments where you can engage with others and exchange ideas. Your curiosity knows no bounds, and you're always eager to learn new things and explore new horizons. Your friendly nature and sharp intellect make you a valued member of any community.",
+      range: [42, 51],
     },
     {
       name: "Sloth",
       description:
         "Ah, you're a Sloth! Easygoing and laid-back, you take life at your own pace, unbothered by the hustle and bustle of the world around you. You know the importance of relaxation and self-care, and you're not afraid to prioritize your well-being above all else. Your calm demeanor and carefree attitude bring a sense of peace to those around you.",
+      range: [52, 61],
     },
     {
       name: "Corgi",
       description:
         "You're a Corgi! Known for your intelligence and loyalty, you approach life with a playful and inquisitive spirit. Your boundless energy and cheerful demeanor make you a joy to be around, and your unwavering loyalty makes you a steadfast friend. Your unique charm and lovable nature make you impossible to resist.",
+      range: [62, 72]
     },
   ];
 
